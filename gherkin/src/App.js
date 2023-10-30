df1bbf0fd7faf1d9dbf8a2afe223ae24843f65df
kp2129 <kristapspolis0@gmail.com>
2023-10-30 13:47:16 +0200

    react Gherkin termini

---
+++ gherkin/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Gherkin termini</h1>
+      <table> 
+        <thead>
+          <tr>
+            <th>Angļu valoda</th>
+            <th>Latviešu valodā</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Value 1</td>
+            <td>Value 2</td>
+          </tr>
+          <tr>
+            <td>Value 3</td>
+            <td>Value 4</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
